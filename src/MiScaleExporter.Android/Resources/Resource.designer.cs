702ccd5e498b47dad852aa3dfcdab1cc50ecc3c6
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-02-19 22:46:24 +0100

    Buildtest version

--- src/MiScaleExporter.Android/Resources/Resource.designer.cs
+++ src/MiScaleExporter.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace MiScaleExporter.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.1.0.11")]
 	public partial class Resource
 	{
 		
