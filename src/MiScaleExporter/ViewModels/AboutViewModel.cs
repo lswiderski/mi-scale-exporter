d4c2011d8a3d80b12ba83f17945838771f35db08
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-02-21 19:07:42 +0100

    Go to scan from about page

--- src/MiScaleExporter/ViewModels/AboutViewModel.cs
+++ src/MiScaleExporter/ViewModels/AboutViewModel.cs
@@ -9,12 +9,15 @@ public class AboutViewModel : BaseViewModel
     public AboutViewModel()
     {
         this.Title = "About";
+        GoToScanCommand = new Command(async () =>
+            await Shell.Current.GoToAsync("///ScalePage"));
         OpenGithubCommand = new Command(async () =>
             await Browser.OpenAsync("https://github.com/lswiderski/mi-scale-exporter"));
         OpenCoffeeCommand = new Command(async () =>
             await Browser.OpenAsync("https://www.buymeacoffee.com/lukaszswiderski"));
     }
     
+    public ICommand GoToScanCommand { get; }
     public ICommand OpenGithubCommand { get; }
     public ICommand OpenCoffeeCommand { get; }
 }
