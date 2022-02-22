cd9bd6b5f383a01cd3282290208a3f23dc753683
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-02-22 18:44:32 +0100

    API address changed to https

--- src/MiScaleExporter/Models/SettingKeys.cs
+++ src/MiScaleExporter/Models/SettingKeys.cs
@@ -2,5 +2,5 @@
 
 public static class SettingKeys
 {
-    public static string ApiServerAddress = "http://srv09.mikr.us:20366";
+    public static string ApiServerAddress = "https://garmin.bieda.it/";
 }
