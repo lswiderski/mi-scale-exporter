2e03374629227b1d42b4551512c3dbb9acadda6b
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-09-01 22:03:36 +0200

    Implemented Direct Garmin Connect call

--- src/MiScaleExporter.MAUI/Services/GarminService.cs
+++ src/MiScaleExporter.MAUI/Services/GarminService.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using GarminWeightScaleUploader.Lib.Models;
 using MiScaleExporter.Models;
 using Newtonsoft.Json;
  
@@ -15,6 +16,8 @@ public class GarminService : IGarminService
     private HttpClient _httpClient;
     private ILogService _logService;
 
+    private readonly string _tmpDir;
+
     public GarminService(ILogService logService)
     {
         _logService = logService;
@@ -22,22 +25,64 @@ public class GarminService : IGarminService
         {
             Timeout = TimeSpan.FromMinutes(5),
         };
+        _tmpDir = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tmp")).FullName;
     }
 
     public async Task<GarminApiResponse> UploadAsync(BodyComposition bodyComposition, DateTime time, string email, string password)
     {
         if(Preferences.Get(PreferencesKeys.UseExternalAPI, false))
         {
-            // TODO switch to internal update
             return await UploadViaExternalAPIAsync(bodyComposition, time, email, password);
         }
         else
         {
-            return await UploadViaExternalAPIAsync(bodyComposition, time, email, password);
+            return await UploadViaDirectCallToGarminAsync(bodyComposition, time, email, password);
         }
     }
 
-    public async Task<GarminApiResponse> UploadViaExternalAPIAsync(BodyComposition bodyComposition, DateTime time, string email, string password)
+    private async Task<GarminApiResponse> UploadViaDirectCallToGarminAsync(BodyComposition bodyComposition, DateTime time, string email, string password)
+    {
+        var result = new GarminApiResponse();
+        try
+        {
+           
+            var userProfileSettings = new UserProfileSettings
+            {
+                Age = 40,
+                Height = 180,
+            };
+            var scaleDTO = new GarminWeightScaleDTO
+            {
+                TimeStamp = time,
+                Weight = Convert.ToSingle(bodyComposition.Weight),
+                PercentFat = Convert.ToSingle(bodyComposition.Fat),
+                PercentHydration = Convert.ToSingle(bodyComposition.WaterPercentage),
+                BoneMass = Convert.ToSingle(bodyComposition.BoneMass),
+                MuscleMass = Convert.ToSingle(bodyComposition.MuscleMass),
+                VisceralFatRating = Convert.ToByte(bodyComposition.VisceralFat),
+                VisceralFatMass = Convert.ToSingle(bodyComposition.VisceralFat),
+                PhysiqueRating = Convert.ToByte(bodyComposition.BodyType),
+                MetabolicAge = Convert.ToByte(bodyComposition.MetabolicAge),
+                BodyMassIndex = Convert.ToSingle(bodyComposition.BMI),
+                Email = email,
+                Password = password
+            };
+
+            var data = scaleDTO with { Email = email, Password = password };
+            var garminApiReponse = await GarminWeightScaleUploader.Lib.GarminWeightScaleUploader.UploadAsync(data, userProfileSettings);
+            result.IsSuccess = garminApiReponse;
+            return result;
+        }
+        catch(Exception ex)
+        {
+            _logService.LogError(ex.Message);
+            result.Message = ex.Message;
+            return result;
+        }
+       
+    }
+
+    private async Task<GarminApiResponse> UploadViaExternalAPIAsync(BodyComposition bodyComposition, DateTime time, string email, string password)
     {
         var unixTime = ((DateTimeOffset) time).ToUnixTimeSeconds();
         var request = new GarminBodyCompositionRequest
